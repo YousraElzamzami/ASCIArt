ed57b4999bfb3b181b29a1357a238ac17f0891c0
YousraElzamzami <yyousra290@gmail.com>
2024-06-17 19:03:19 -0400

    Adjustment to main page

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ ASCII_CHARS = "@%#*+=-:. "
 
 def resize_image(image, new_width=100):
     width, height = image.size
-    ratio = height / width / 1.65  # Adjust ratio for correct aspect
+    ratio = height / width / 1.65  
     new_height = int(new_width * ratio)
     resized_image = image.resize((new_width, new_height))
     return resized_image
